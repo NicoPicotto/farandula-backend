3b102f85c04fccc14ec290485b307527fccdf221
Nicolas Picotto <nicopicotto@hotmail.com>
2025-06-20 10:28:20 -0300

    amplie tiempo del token

--- src/utils/jtw.ts
+++ src/utils/jtw.ts
@@ -6,7 +6,7 @@ import jwt from "jsonwebtoken";
 const SECRET = process.env.JWT_SECRET || "default_secret";
 
 export const generateToken = (userId: string): string => {
-   return jwt.sign({ userId }, SECRET, { expiresIn: "1h" });
+   return jwt.sign({ userId }, SECRET, { expiresIn: "30d" });
 };
 
 export const verifyToken = (token: string): any => {
